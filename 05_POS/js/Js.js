d2ce54add138ed6aa88aa5898dcd88f7c5cf3432
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-08-09 15:03:45 +0530

    updated

---
+++ 05_POS/js/Js.js
@@ -0,0 +1,57 @@
+/*  document.getElementById("btnHome").addEventListener("click", function () {
+               $("#Home").css({display: 'block'})
+               $("#Item").css({display: 'none'})
+               $("#Customer").css({display: 'none'})
+
+           })*/
+
+/*
+  document.getElementById("btnOrder").addEventListener("click", function () {
+      $("#Home").css({display: 'block'})
+      $("#Item").css({display: 'none'})
+      $("#Customer").css({display: 'none'})
+  })*/
+
+/*document.getElementById("btnItems").addEventListener("click", function () {
+      $("#Home").css({display: 'none'})
+      $("#Item").css({display: 'block'})
+      $("#Customer").css({display: 'none'})
+  })
+*/
+/*document.getElementById("btnCustomer").addEventListener("click", function () {
+   $("#Home").css({display: 'none'})
+   $("#Item").css({display: 'none'})
+   $("#Customer").css({display: 'block'})
+})*/
+
+$("#btnHome").click(function (){
+    $("#Home").css({display: 'block'})
+    $("#Item").css({display: 'none'})
+    $("#Customer").css({display: 'none'})
+})
+
+$("#btnOrder").click(function (){
+    $("#Home").css({display: 'block'})
+    $("#Item").css({display: 'none'})
+    $("#Customer").css({display: 'none'})
+})
+
+$("#btnItems").click(function (){
+    $("#Home").css({display: 'none'})
+    $("#Item").css({display: 'block'})
+    $("#Customer").css({display: 'none'})
+})
+
+$("#btnCustomer").click(function (){
+    $("#Home").css({display: 'none'})
+    $("#Item").css({display: 'none'})
+    $("#Customer").css({display: 'block'})
+})
+
+
+/*-------------------------Customer section--------------------------*/
+
+/*---save button------*/
+$("#btnSave").click(function (){
+
+})
