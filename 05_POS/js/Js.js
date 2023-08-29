c15e2d6f7f77b14c5d07367a76eb28371f114a7c
Kalana Dananjaya <kalanadananjaya855@gmail.com>
2023-08-29 22:43:19 +0530

    updated

--- 05_POS/js/Js.js
+++ 05_POS/js/Js.js
@@ -127,13 +127,13 @@ $("#txtAddress").keydown(function (event){
         $("#btnSave").click();
     }
 })
-
-/*--To stop the task and execution of the key*/
-$("#txtCId,#txtSalary,#txtAddress").keydown(function (event){
+/*
+/!*--To stop the task and execution of the key*!/
+$("#txtCId,#txtSalary,#txtAddress").keydown(function (eventOb){
     if(eventOb.key == "K"){
         eventOb.preventDefault();//execute the task
     }
-})
+})*/
 
 /*--Regular Expressions---*/
 /*var regExCusID =/^(C)[0-9]{3}$/;
